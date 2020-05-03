d80b5676f2bc42d75a038c1517bc93520174e482
Christopher Zenner <christopher.zenner@gmail.com>
2020-05-03 16:25:54 -0700

    style: add styling to terminal output via chalk

--- bamazon-mgr.js
+++ bamazon-mgr.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 
-const inquirer = require('inquirer'),
+const _ = require('lodash'),
+      chalk = require('chalk'),
+      inquirer = require('inquirer'),
       keys = require('./keys.js'),
       mysql = require('mysql');
 
@@ -15,11 +17,25 @@ const conn = mysql.createConnection({
 conn.connect((err) => {
   if (err) throw err;
 
-  conn.query('SELECT * FROM products;', (err, res) => {
+  const welcomeMsg = chalk`Welcome to {bold.hex('#FF8C00') bamazon}, the online store for {italic all} your shopping needs!\nHere\'s a list of the incredible deals we have for you today:\n`;
+
+  console.log(welcomeMsg);
+
+  conn.query('SELECT item_id, product_name, price FROM products;', (err, res) => {
     if (err) throw err;
 
+    const itemNoHeader = chalk`{bold.hex('#C0C0C0').underline Item No.  }`,
+          productHeader = chalk`{bold.hex('#C0C0C0').underline Product                                                                 }`,
+          priceHeader = chalk`{bold.hex('#C0C0C0').underline Price     }`,
+          spacer = `     `;
+
+    console.log(`${itemNoHeader}${spacer}${productHeader}${spacer}${priceHeader}`);
+
     for (let row of res) {
-      console.log(`${row.product_name} - $${row.price}`);
+      const productName = chalk`{hex('#FFA500') ${_.padEnd(row.product_name.slice(0, 72), 72)}}`,
+            price = _.padStart(row.price, 9);
+
+      console.log(`${row.item_id}${spacer}${productName}${spacer}$${price}`);
     }
   });
 
