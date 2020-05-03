d7f08f0463948c477da39132b014a87e538cc4fa
Christopher Zenner <christopher.zenner@gmail.com>
2020-05-02 19:51:57 -0700

    test: seed bamazon db with test data

---
+++ bamazon-data.sql
@@ -0,0 +1,13 @@
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES 
+  (1015984230, 'VIZIO M-Series Quantum 55” Class 4K HDR Smart TV (M558-G1)', 'Appliances & Electronics', 528.99, 25),
+  (2016983235, 'Reality Is Not What It Seems: The Journey to Quantum Gravity', 'Books & Audiobooks', 17.89, 125),
+  (3669425498, 'Acer Spin 3 Convertible 14" Laptop with 8th Gen Intel Core i7 (SP314-53N-77AJ)', 'Computers & Tech', 799.99, 13),
+  (4619249854, "Bruno Marc Men's Military Motorcycle Combat Boots", 'Clothing & Accessories', 39.99, 67),
+  (5889422132, 'GoMacro Macrobars 2.4 oz. (Peanut Butter Chocolate Chip)', 'Food & Groceries', 2.79, 242),
+  (6662492486, 'Grave Before Shave Pine/Cedar Wood Beard Oil', 'Health & Beauty', 13.99, 189),
+  (7895498624, 'Casper Original Foam Mattress, Queen, 2020 Model', 'Housewares & Furniture', 985.59, 7),
+  (8989425366, 'Organic Wheat Grass Seeds 16 oz. (Hard Red Wheat)', 'Lawn & Garden', 11.97, 543),
+  (9542316098, "God's Own Country (DVD)", 'Movies & Music', 12.57, 92),
+  (1078246598, 'Blue Buffalo Life Protection Formula Natural Adult Dry Dog Food 30 lb.', 'Pet Supplies', 48.99, 39);
+  
